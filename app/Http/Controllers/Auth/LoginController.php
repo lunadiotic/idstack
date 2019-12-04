647ffaff6d4417b86eb7e423b78bb1787e7efb34
vagrant <vagrant@homestead>
2019-12-04 14:23:03 +0000

    Modify Auth with Username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -36,4 +37,51 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        $field = $this->field($request);
+        return [
+            $field => $request->get($this->username()),
+            'password' => $request->get('password')
+        ];
+    }
+
+    /**
+     * Determine if the request field is email or username
+     *
+     * @param Request $request
+     * @return string
+     */
+    public function field(Request $request)
+    {
+        $email = $this->username();
+        return filter_var($request->get('email'), FILTER_VALIDATE_EMAIL) ? $email : 'username';
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function validateLogin(Request $request)
+    {
+        $field = $this->field($request);
+        $messages = ["{$this->username()}.exists" => 'The account you are trying to login is not registered or it has been disabled.'];
+        $this->validate($request, [
+            $this->username() => ["required", "exists:users,{$field}"],
+            'password' => 'required'
+        ], $messages);
+    }
+
+
 }
